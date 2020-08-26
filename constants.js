9ae07d8ccf4fd8222f13642f958bfedc227fa943
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-26 21:56:18 +0000

    implemented constants and put messages into a random array

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+
+const UPKEY = "Move: up";
+const LEFTKEY = "Move: left";
+const DOWNKEY = "Move: down";
+const RIGHTKEY = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  UPKEY,
+  LEFTKEY,
+  DOWNKEY,
+  RIGHTKEY
+};
--- input.js
+++ input.js
@@ -1,45 +1,65 @@
-const { stdout } = require("process");
+const { UPKEY, LEFTKEY, DOWNKEY, RIGHTKEY } = require('./constants.js');
+
 // Stores the active TCP connection object.
 let connection;
-
+const randomMessages = ["Go Snake Go!", " Must get the precious....!", "Charrrrge!!!!"]
+let timer;
 //handleUserInput registered as the "data" callback handler for stdin
 const handleUserInput = (data) => {
+  const stdout = process.stdout;
+  const interval = function (data) {
+    timer = setInterval(() => {
+      connection.write(data);
+    }, 15)
+  };
   // console.log(data)
   if (data === "\u0003") {
+    stdout.write("Left the game, au revoir \n")
     process.exit();
   }
 
   // test experiment
-  if(data === "w"){
+  if (data === "w") {
     // console.log('you hit the w key!')
     //passing the string "Move: up"
-    connection.write("Move: up");
+    // connection.write("Move: up");
+    clearInterval(timer);
+    interval(UPKEY);
   }
 
-  if(data === "s"){
+  if (data === "s") {
     // console.log('you hit the s key!')
-    connection.write("Move: down");
+    // connection.write("Move: down");
+    clearInterval(timer);
+    interval(DOWNKEY);
   }
 
-  if(data === "a"){
+  if (data === "a") {
     // console.log('you hit the a key!')
-    connection.write("Move: left");
+    // connection.write("Move: left");
+    clearInterval(timer);
+    interval(LEFTKEY);
   }
 
-  if(data === "d"){
+  if (data === "d") {
     // console.log('you hit the d key!')
-    connection.write("Move: right");
+    // connection.write("Move: right");
+    clearInterval(timer);
+    interval(RIGHTKEY);
   }
 
-  if(data === "r"){
+  if (data === "r") {
     // console.log('you hit the d key!')
-    connection.write("Say: Go Snake Go!");
-  }
+    let randomMsg = Math.floor(Math.random() * randomMessages.length)
+    connection.write(`Say: ${randomMessages[randomMsg]}`);
+    // console.log(randomMsg)
+    // console.log(randomMessages.length)
+  };
 
-  if(data === "q"){
-    // console.log('you hit the d key!')
-    connection.write("Say: Must get the precious....!");
-  }
+  // if(data === "q"){
+  //   // console.log('you hit the d key!')
+  //   connection.write("Say: Must get the precious....!");
+  // }
 };
 
 /**
@@ -61,4 +81,4 @@ const setupInput = function (conn) {
   return stdin;
 }
 
-module.exports = {setupInput}
+module.exports = { setupInput }
